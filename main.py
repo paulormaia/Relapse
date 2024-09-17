6a35d3ca5b508bb1800dfb0044815d7340ee2c9a
Paulo <108302310+paulormaia@users.noreply.github.com>
2024-09-17 23:41:24 +0000

    adicionando agendamento de consultas

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def perfil():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3000, debug = True)
+  app.run(host='0.0.0.0', port=8888, debug = True)
---
+++ settings.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = '2D1z7D5n07p94oBrgsjuq6YgCyR6sNcGmNpsOZSP';
+var REDIRECT_URI = 'http://improved-doodle-qjv5wgxw9qj29454-8888.app.github.dev/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
